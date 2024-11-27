52b1c9cccdf24f47ef1b7a4474e19cc83a948697
JulikaKhoroshavina <julika151185@gmail.com>
2024-11-27 20:41:35 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Julia': 1985, 'Mathew': 2016}
+print (my_dict)
+print (my_dict.get('Julia'))
+print (my_dict.get('Vasilisa'))
+my_dict.update({'Daria': 2003,'Michael': 1978})
+x=my_dict.pop ('Julia')
+print(x)
+print (my_dict)
+my_set = {1,2,3,5,'Julia',5,3,2,1,'Julia'}
+print(my_set)
+my_set.add(7),my_set.add(10)
+my_set.discard(10)
+print(my_set)
