3e60ee3e1a5072199bd881beae147153e9645098
JulikaKhoroshavina <julika151185@gmail.com>
2025-01-24 19:01:58 +0300

    Update module2_2.py

--- module2_2.py
+++ module2_2.py
@@ -1,9 +1,9 @@
 number_first=int(input('Введите число: '))
 number_second=int(input('Введите число: '))
 number_third=int(input('Введите число: '))
-if number_first==number_second and number_first==number_third:
+if number_first==number_second and number_second==number_third:
     print(3)
-elif number_first==number_second or number_first==number_third:
+elif number_first==number_second or number_second==number_third or number_third==number_first:
     print(2)
 else:
     print(0)
